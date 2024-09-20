3201d12b9cab6ca5cebe108961b9f19a0072bebd
pratik.samrat <pratik.samrat@bizsense.com>
2024-09-20 11:51:54 +0530

    old browser support

--- js/tv-menu-element.js
+++ js/tv-menu-element.js
@@ -48,54 +48,48 @@ $(document).ready(function () {
 });
 
 function detectPlatformAndBrowser() {
-  const userAgent = navigator.userAgent.toLowerCase();
-  let platform = "Unknown Platform";
-  let browserInfo = "Unknown Browser";
+  var userAgent = navigator.userAgent.toLowerCase();
+  var platform = "Unknown Platform";
+  var browserInfo = "Unknown Browser";
 
   // Detect TV platforms
-  if (userAgent.includes("android")) {
+  if (userAgent.indexOf("android") !== -1) {
     platform = "Android TV";
-  } else if (userAgent.includes("amino")) {
+  } else if (userAgent.indexOf("amino") !== -1) {
     platform = "Amino TV";
-  } else if (userAgent.includes("infomir")) {
+  } else if (userAgent.indexOf("infomir") !== -1) {
     platform = "Infomir TV";
-  } else if (userAgent.includes("samsung")) {
+  } else if (userAgent.indexOf("samsung") !== -1) {
     platform = "Samsung Smart TV";
-  } else if (userAgent.includes("sony")) {
+  } else if (userAgent.indexOf("sony") !== -1) {
     platform = "Sony Smart TV";
-  } else if (
-    userAgent.includes("windows") ||
-    userAgent.includes("macintosh") ||
-    userAgent.includes("linux")
-  ) {
+  } else if (userAgent.indexOf("windows") !== -1 || userAgent.indexOf("macintosh") !== -1 || userAgent.indexOf("linux") !== -1) {
     platform = "PC Browser";
   }
 
   // Detect browser and version
-  if (userAgent.includes("chrome") && !userAgent.includes("edg")) {
-    const match = userAgent.match(/chrome\/(\d+)/);
-    browserInfo = match ? `Chrome version ${match[1]}` : "Chrome";
-  } else if (userAgent.includes("firefox")) {
-    const match = userAgent.match(/firefox\/(\d+)/);
-    browserInfo = match ? `Firefox version ${match[1]}` : "Firefox";
-  } else if (userAgent.includes("safari") && !userAgent.includes("chrome")) {
-    const match = userAgent.match(/version\/(\d+)/);
-    browserInfo = match ? `Safari version ${match[1]}` : "Safari";
-  } else if (userAgent.includes("opera") || userAgent.includes("opr")) {
-    const match = userAgent.match(/(opera|opr)\/(\d+)/);
-    browserInfo = match ? `Opera version ${match[2]}` : "Opera";
-  } else if (userAgent.includes("msie") || userAgent.includes("trident")) {
-    const match = userAgent.match(/(msie|rv:)(\d+)/);
-    browserInfo = match
-      ? `Internet Explorer version ${match[2]}`
-      : "Internet Explorer";
-  } else if (userAgent.includes("edg")) {
-    const match = userAgent.match(/edg\/(\d+)/);
-    browserInfo = match ? `Edge version ${match[1]}` : "Edge";
+  if (userAgent.indexOf("chrome") !== -1 && userAgent.indexOf("edg") === -1) {
+    var chromeMatch = userAgent.match(/chrome\/(\d+)/);
+    browserInfo = chromeMatch ? "Chrome version " + chromeMatch[1] : "Chrome";
+  } else if (userAgent.indexOf("firefox") !== -1) {
+    var firefoxMatch = userAgent.match(/firefox\/(\d+)/);
+    browserInfo = firefoxMatch ? "Firefox version " + firefoxMatch[1] : "Firefox";
+  } else if (userAgent.indexOf("safari") !== -1 && userAgent.indexOf("chrome") === -1) {
+    var safariMatch = userAgent.match(/version\/(\d+)/);
+    browserInfo = safariMatch ? "Safari version " + safariMatch[1] : "Safari";
+  } else if (userAgent.indexOf("opera") !== -1 || userAgent.indexOf("opr") !== -1) {
+    var operaMatch = userAgent.match(/(opera|opr)\/(\d+)/);
+    browserInfo = operaMatch ? "Opera version " + operaMatch[2] : "Opera";
+  } else if (userAgent.indexOf("msie") !== -1 || userAgent.indexOf("trident") !== -1) {
+    var ieMatch = userAgent.match(/(msie|rv:)(\d+)/);
+    browserInfo = ieMatch ? "Internet Explorer version " + ieMatch[2] : "Internet Explorer";
+  } else if (userAgent.indexOf("edg") !== -1) {
+    var edgeMatch = userAgent.match(/edg\/(\d+)/);
+    browserInfo = edgeMatch ? "Edge version " + edgeMatch[1] : "Edge";
   }
 
   // Output platform and browser information
-  alert(`Platform: ${platform}\nBrowser: ${browserInfo}`);
+  alert("Platform: " + platform + "\nBrowser: " + browserInfo);
 }
 
 // helpers
